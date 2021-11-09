e031d6e8153309efc27b186c80364ef57e9e111d
kaysonwu <772663139@qq.com>
2021-11-09 21:48:59 +0800

    feta: update `Password` field

--- packages/field/src/components/password/index.tsx
+++ packages/field/src/components/password/index.tsx
@@ -1,17 +1,75 @@
-import React, { forwardRef } from 'react';
-import { Input } from 'antd';
+import React, { ReactElement, cloneElement, forwardRef, useContext, useState } from 'react';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+import { Input, InputProps } from 'antd';
+import { ConfigContext } from 'antd/lib/config-provider';
+import classNames from 'classnames';
 import type { FieldFC, FieldProps as Props } from '../../typings';
-import type { PasswordProps as InputProps } from 'antd/lib/input';
 
-export interface PasswordProps extends Omit<InputProps, keyof Props>, Props<string> {}
+export interface PasswordProps extends Omit<InputProps, keyof Props | 'suffix'>, Props<string> {
+  /** 默认是否可见 */
+  defaultVisible?: boolean;
+  /** 隐藏标记 */
+  hiddenMark?: string;
+  /** 自定义切换按钮 */
+  iconRender?: (visible: boolean) => ReactElement;
+  /** 是否显示切换按钮 */
+  visibilityToggle?: boolean;
+}
 
-const Password: FieldFC<PasswordProps> = ({ defaultValue, readOnly, value, ...props }, ref) => {
+const Password: FieldFC<PasswordProps> = (
+  {
+    defaultValue,
+    defaultVisible = false,
+    disabled,
+    className,
+    hiddenMark = '＊＊＊＊＊＊',
+    iconRender = visible => (visible ? <EyeOutlined /> : <EyeInvisibleOutlined />),
+    plain: _,
+    prefixCls: customizePrefixCls,
+    readOnly,
+    size,
+    value,
+    visibilityToggle = true,
+    ...props
+  },
+  ref,
+) => {
+  const [visible, setVisible] = useState(defaultVisible);
+  const { getPrefixCls } = useContext(ConfigContext);
+
+  const prefixCls = getPrefixCls('input-password', customizePrefixCls);
+  const suffix =
+    visibilityToggle &&
+    cloneElement(iconRender(visible), {
+      key: 'passwordIcon',
+      className: `${prefixCls}-icon`,
+      onClick: disabled ? undefined : () => setVisible(!visible),
+      onMouseDown: (e: MouseEvent) => e.preventDefault(),
+      onMouseUp: (e: MouseEvent) => e.preventDefault(),
+    });
+
   if (readOnly) {
-    return <span ref={ref}>{value === undefined ? defaultValue : value}</span>;
+    return (
+      <span ref={ref}>
+        {visible ? value || defaultValue || '-' : hiddenMark}
+        {suffix}
+      </span>
+    );
   }
 
   return (
-    <Input.Password allowClear {...props} ref={ref} defaultValue={defaultValue} value={value} />
+    <Input
+      allowClear
+      {...props}
+      ref={ref}
+      className={classNames(prefixCls, className, { [`${prefixCls}-${size}`]: !!size })}
+      defaultValue={defaultValue}
+      disabled={disabled}
+      size={size}
+      suffix={suffix}
+      type={visible ? 'text' : 'password'}
+      value={value}
+    />
   );
 };
 
