75a10501fe1c2fbbb862231791df67eee5aed8f5
kaysonwu <772663139@qq.com>
2021-11-09 22:14:07 +0800

    feta: update `Digit` & `Money` Field

--- packages/field/src/components/digit/index.tsx
+++ packages/field/src/components/digit/index.tsx
@@ -1,15 +1,63 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useContext, useMemo } from 'react';
 import { InputNumber, InputNumberProps } from 'antd';
+import { ConfigContext } from 'antd/lib/config-provider';
 import type { FieldFC, FieldProps as Props } from '../../typings';
 
-export interface DigitProps extends Omit<InputNumberProps, keyof Props>, Props<string | number> {}
+export interface DigitProps extends Omit<InputNumberProps, keyof Props>, Props<string | number> {
+  /** 格式化选项 */
+  formatOptions?: Intl.NumberFormatOptions;
+  /** 语言代码，例如：zh-cn, en-us */
+  locale?: string;
+}
 
-const Digit: FieldFC<DigitProps> = ({ defaultValue, readOnly, value, ...props }, ref) => {
+const Digit: FieldFC<DigitProps> = (
+  { defaultValue, formatOptions, locale: customizeLocale, plain: _, readOnly, value, ...props },
+  ref,
+) => {
+  const config = useContext(ConfigContext);
+  const locale = customizeLocale !== undefined ? customizeLocale : config.locale?.locale;
+
+  const formatter = useMemo(() => new Intl.NumberFormat(locale, formatOptions), [
+    locale,
+    formatOptions,
+  ]);
+
+  const parser = (val: string | undefined) => {
+    if (!val) {
+      return 0;
+    }
+
+    const localeFormatter = new Intl.NumberFormat(locale);
+
+    // detecting and parsing between comma and dot
+    const group = localeFormatter.format(1111).replace(/1/g, '');
+    const decimal = localeFormatter.format(1.1).replace(/1/g, '');
+
+    return val
+      .replace(new RegExp(`\\${group}`, 'g'), '')
+      .replace(new RegExp(`\\${decimal}`, 'g'), '.')
+      .replace(/[^0-9.]/g, '');
+  };
+
   if (readOnly) {
-    return <span ref={ref}>{value === undefined ? defaultValue : value}</span>;
+    return (
+      <span ref={ref}>
+        {formatter.format((value === undefined ? defaultValue : value) as number)}
+      </span>
+    );
   }
 
-  return <InputNumber min={0} {...props} ref={ref} defaultValue={defaultValue} value={value} />;
+  return (
+    <InputNumber
+      min={0}
+      {...props}
+      ref={ref}
+      defaultValue={defaultValue}
+      formatter={formatter.format as InputNumberProps['formatter']}
+      parser={parser}
+      value={value}
+    />
+  );
 };
 
 export default forwardRef(Digit);
--- packages/field/src/components/money/index.tsx
+++ packages/field/src/components/money/index.tsx
@@ -1,11 +1,8 @@
-import React, { forwardRef, useContext, useMemo } from 'react';
-import { InputNumber, InputNumberProps } from 'antd';
-import { ConfigContext } from 'antd/lib/config-provider';
-import type { FieldFC, FieldProps as Props } from '../../typings';
+import React, { forwardRef } from 'react';
+import Digit, { DigitProps } from '../digit';
+import type { FieldFC } from '../../typings';
 
-export interface MoneyProps extends Omit<InputNumberProps, keyof Props>, Props<string | number> {
-  /** 语言代码，例如：zh-cn, en-us */
-  locale?: string;
+export interface MoneyProps extends Omit<DigitProps, 'formatOptions'> {
   /** 货币代码，例如：CNY, USD */
   currency?: string;
   /** 货币显示格式  */
@@ -13,68 +10,20 @@ export interface MoneyProps extends Omit<InputNumberProps, keyof Props>, Props<s
 }
 
 const Money: FieldFC<MoneyProps> = (
-  {
-    currency = 'CNY',
-    currencyDisplay = 'narrowSymbol',
-    defaultValue,
-    locale: customizeLocale,
-    plain: _,
-    precision = 2,
-    readOnly,
-    value,
-    ...props
-  },
+  { currency = 'CNY', currencyDisplay = 'narrowSymbol', precision = 2, ...props },
   ref,
-) => {
-  const config = useContext(ConfigContext);
-  const locale = customizeLocale !== undefined ? customizeLocale : config.locale?.locale;
-
-  const numberFormat = useMemo(
-    () =>
-      new Intl.NumberFormat(locale, {
-        style: 'currency',
-        currency,
-        currencyDisplay,
-        minimumFractionDigits: precision,
-      }),
-    [locale, currency, currencyDisplay, precision],
-  );
-
-  const parser = (val: string | undefined) => {
-    if (!val) {
-      return 0;
-    }
-
-    const localeNumberFormat = new Intl.NumberFormat(locale);
-
-    // detecting and parsing between comma and dot
-    const group = localeNumberFormat.format(1111).replace(/1/g, '');
-    const decimal = localeNumberFormat.format(1.1).replace(/1/g, '');
-
-    return val
-      .replace(new RegExp(`\\${group}`, 'g'), '')
-      .replace(new RegExp(`\\${decimal}`, 'g'), '.')
-      .replace(/[^0-9.]/g, '');
-  };
-
-  if (readOnly) {
-    return (
-      <span ref={ref}>
-        {numberFormat.format((value === undefined ? defaultValue : value) as number)}
-      </span>
-    );
-  }
-
-  return (
-    <InputNumber
-      {...props}
-      ref={ref}
-      defaultValue={defaultValue}
-      formatter={numberFormat.format as InputNumberProps['formatter']}
-      parser={parser}
-      value={value}
-    />
-  );
-};
+) => (
+  <Digit
+    {...props}
+    ref={ref}
+    formatOptions={{
+      style: 'currency',
+      currency,
+      currencyDisplay,
+      minimumFractionDigits: precision,
+    }}
+    precision={precision}
+  />
+);
 
 export default forwardRef(Money);
